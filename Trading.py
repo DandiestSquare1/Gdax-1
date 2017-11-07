ae297e5011e9878fb708f229379445487767c19b
ruohanliu <ruohan.lrh@gmail.com>
2017-11-06 23:13:14 -0800

    more index

--- Trading.py
+++ Trading.py
@@ -119,14 +119,14 @@ async def compute_spread(unit):
 		# Giving price closer to the center more weight
 		Adjusted_Volume = 0
 		for price in order_book.irange(current_price * (1 - fluctuation), current_price, (True,True)):
-			temp = (abs(price - current_price) / current_price / fluctuation + 0.5) * order_book[price]
+			temp = (abs(price - current_price) / current_price / fluctuation + 0.1) * order_book[price]
 			Adjusted_Volume += temp
 			Bid_Index += temp * price
 		Bid_Index = round(Bid_Index / Adjusted_Volume,2)
 		
 		Adjusted_Volume = 0
 		for price in order_book.irange(current_price + 0.01, current_price * (1 + fluctuation), (True,True)):
-			temp = (abs(price - current_price) / current_price / fluctuation + 0.5) * order_book[price]
+			temp = (abs(price - current_price) / current_price / fluctuation + 0.1) * order_book[price]
 			Adjusted_Volume += temp
 			Ask_Index += temp * price
 		Ask_Index = round(Ask_Index / Adjusted_Volume,2)
@@ -138,18 +138,19 @@ async def compute_spread(unit):
 loop = asyncio.get_event_loop()
 # loop.create_task(get_wss_order_book(5))
 # loop.create_task(get_wss_match_book(5))
-loop.run_until_complete(asyncio.wait({get_wss_order_book(7200),get_wss_match_book(),compute_spread(1),compute_spread(30)}))
+loop.run_until_complete(asyncio.wait({get_wss_order_book(3600*9),get_wss_match_book(),compute_spread(1),compute_spread(30),compute_spread(120)}))
 loop.close()
 
 def export_interval_data():
-	# Create lists of time intervals containing interval level info: (Time, Low, Weighted_Price_Average, High, N_Transactions, Volume, Bid_Index, Ask_Index, EMA_12, EMA_26, EMA_DIF, MACD)
+	# Create lists of time intervals containing interval level info: (Time, trailing_price, Low, Weighted_Price_Average, High, close_price, N_Transactions, Volume, Bid_Index, Ask_Index, EMA_12, EMA_26, EMA_DIF, MACD, EMA_Up,EMA_Down,RSI, TSI, EMA_Volume, OBV_Delta)
 	# If an interval has 0 transaction then the interval list has 0 Volume and the prices is the trailing price
 	# Time is epoch time
 	global match_book,interval_book,Start_Time,End_Time,start_price
+	sign = lambda x: (x>0) - (x<0)
 
 	for unit in interval_book.keys():
 		text_file = open("Interval_Data_by_%s_Seconds.txt" % (unit), "w")
-		
+		text_file.write("Time\ttrailing_price\tLow\tWeighted_Price_Average\tHigh\tclose_price\tN_Transactions\tVolume\tBid_Index\tAsk_Index\tEMA_12\tEMA_26\tEMA_DIF\tMACD\tEMA_Up\tEMA_Down\tRSI\tTSI\tEMA_Volume\tOBV_Delta\n")
 		trailing_price = start_price
 		match_book_index = 0
 
@@ -164,9 +165,9 @@ def export_interval_data():
 			High = 0
 
 			while match_book_index < len(match_book) and match_book[match_book_index][2] < Time + unit:
-				trailing_price = match_book[match_book_index][0]
-				Low = min(Low,trailing_price)
-				High = max(High,trailing_price)
+				close_price = match_book[match_book_index][0]
+				Low = min(Low,close_price)
+				High = max(High,close_price)
 				Volume += match_book[match_book_index][1]
 				N_Transactions += 1
 				Amount += match_book[match_book_index][0] * match_book[match_book_index][1]
@@ -178,18 +179,40 @@ def export_interval_data():
 				Low = trailing_price
 				High = trailing_price
 				Weighted_Price_Average = trailing_price
+				close_price = trailing_price
 
 			if period == 1:
 				EMA_12 = Weighted_Price_Average
 				EMA_26 = Weighted_Price_Average
 				EMA_DIF = 0
+				EMA_Up = max(0,close_price - trailing_price)
+				EMA_Down = max(0,trailing_price - close_price)
+				EMA_Momentum = close_price - trailing_price
+				EMA_Momentum_Abs = abs(close_price - trailing_price)
+				EMA_Momentum_2 = EMA_Momentum
+				EMA_Momentum_Abs_2 = EMA_Momentum_Abs
+				EMA_Volume = Volume
+				OBV_Delta = 0
 			else:
-				EMA_12 = (1-2/(12+1)) * EMA_12 + 2/(12+1) * Weighted_Price_Average
-				EMA_26 = (1-2/(26+1)) * EMA_26 + 2/(26+1) * Weighted_Price_Average
-				EMA_DIF = (1-2/(9+1)) * EMA_DIF + 2/(9+1) * (EMA_12 - EMA_26)
-			MACD = EMA_12 - EMA_26 - EMA_DIF
+				EMA_12 = round((1-2/(12+1)) * EMA_12 + 2/(12+1) * Weighted_Price_Average,6)
+				EMA_26 = round((1-2/(26+1)) * EMA_26 + 2/(26+1) * Weighted_Price_Average,6)
+				EMA_DIF = round((1-2/(9+1)) * EMA_DIF + 2/(9+1) * (EMA_12 - EMA_26),6)
+				EMA_Up = round((1-1/14) * EMA_Up + 1/14 * max(0,close_price - trailing_price),6)
+				EMA_Down = round((1-1/14) * EMA_Down + 1/14 * max(0,trailing_price - close_price),6)
+				EMA_Momentum = (1-2/(25+1)) * EMA_Momentum + 2/(25+1) * (close_price - trailing_price)
+				EMA_Momentum_Abs = (1-2/(25+1)) * EMA_Momentum_Abs + 2/(25+1) * abs(close_price - trailing_price)
+				EMA_Momentum_2 = (1-2/(13+1)) * EMA_Momentum_2 + 2/(13+1) * EMA_Momentum
+				EMA_Momentum_Abs_2 = (1-2/(13+1)) * EMA_Momentum_Abs_2 + 2/(13+1) * EMA_Momentum_Abs
+				EMA_Volume = round((1-2/(12+1)) * EMA_Volume + 2/(12+1) * Volume,8)
+				# OBV_Delta is generally used to confirm price moves. The idea is that volume is higher on days where the price move is in the dominant direction
+				OBV_Delta = round(Volume * sign(close_price - trailing_price),8)
 
-			text_file.write("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (Time, Low, Weighted_Price_Average, High, N_Transactions, Volume, Bid_Index, Ask_Index, EMA_12, EMA_26, EMA_DIF, MACD))
+			MACD = round(EMA_12 - EMA_26 - EMA_DIF,6)
+			RSI = 50 if EMA_Down == 0 else round((1-1/(1+(EMA_Up/EMA_Down))) * 100,2)
+			TSI = 0 if EMA_Momentum_Abs_2 == 0 else round(EMA_Momentum_2 / EMA_Momentum_Abs_2 * 100,2)
+
+			text_file.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % (Time, trailing_price, Low, Weighted_Price_Average, High, close_price, N_Transactions, Volume, Bid_Index, Ask_Index, EMA_12, EMA_26, EMA_DIF, MACD, EMA_Up,EMA_Down,RSI, TSI, EMA_Volume, OBV_Delta))
+			trailing_price = close_price
 		text_file.close()
 			
 export_interval_data()
